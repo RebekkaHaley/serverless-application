3b35c5f1abfccb3eb50978aca821144d45142feb
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-15 23:15:37 +0100

    [app] moved previous code to getTodo.ts

---
+++ backend/src/lambda/http/getTodo.ts
@@ -0,0 +1,44 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+const todoIdIndex = process.env.TODO_ID_INDEX
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // TODO: Get all TODO items for a current user
+  console.log('Caller event', event)
+  const todoId = event.pathParameters.todoId
+
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: todoIdIndex,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    }
+  }).promise()
+
+  if (result.Count !== 0) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(result.Items[0])
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
+}
