726166a505bd81d35406b519097cbbbc5be17e6c
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-15 22:43:40 +0100

    [app] Created an Auth0 app & copied values over

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,8 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  // Create an Auth0 application and copy values from it into this map
+  domain: 'dev-eb-o8fgn.eu.auth0.com',            // Auth0 domain
+  clientId: 'l3s9l1W1QsyZwKShHVb001Nk8fNkXaJo',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
