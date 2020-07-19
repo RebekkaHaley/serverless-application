b6ec71658e4685e13176ddbc7077f2a824af493e
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-19 23:30:20 +0100

    [app] updated todo access in data layer

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -1,37 +1,86 @@
-import * as AWS  from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-import { Group } from '../models/Group'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate'
 
-export class GroupAccess {
+export class TodoAccess {
 
   constructor(
     private readonly docClient: DocumentClient = createDynamoDBClient(),
-    private readonly groupsTable = process.env.GROUPS_TABLE) {
-  }
+    private readonly todosTable = process.env.TODOS_TABLE,
+    private readonly userIdIndex = process.env.USER_ID_INDEX
+  ) { }
 
-  async getAllGroups(): Promise<Group[]> {
-    console.log('Getting all groups')
+  async getAllTodos(userId: string): Promise<TodoItem[]> {
+    console.log('Getting all todos')
 
     const result = await this.docClient.scan({
-      TableName: this.groupsTable
+      TableName: this.todosTable,
+      FilterExpression: 'userId = :userId',
+      ExpressionAttributeValues: { ':userId': userId }
     }).promise()
 
     const items = result.Items
-    return items as Group[]
+    return items as TodoItem[]
   }
 
-  async createGroup(group: Group): Promise<Group> {
+  async createTodo(todoItem: TodoItem): Promise<TodoItem> {
     await this.docClient.put({
-      TableName: this.groupsTable,
-      Item: group
+      TableName: this.todosTable,
+      Item: todoItem
     }).promise()
 
-    return group
+    return todoItem
   }
+
+  async getTodo(todoId: string, userId: string): Promise<TodoItem> {
+    const result = await this.docClient
+      .query({
+        TableName: this.todosTable,
+        IndexName: this.userIdIndex,
+        KeyConditionExpression: 'todoId = :todoId and userId = :userId',
+        ExpressionAttributeValues: {
+          ':todoId': todoId,
+          ':userId': userId
+        }
+      }).promise()
+
+    const item = result.Items[0]
+    return item as TodoItem
+  }
+
+  async updateTodo(todoId: string, createdAt: string, todoUpdate: TodoUpdate): Promise<void> {
+    this.docClient.update({
+      TableName: this.todosTable,
+      Key: {
+        todoId,
+        createdAt
+      },
+      UpdateExpression: 'set #n = :name, done = :done, dueDate = :dueDate',
+      ExpressionAttributeValues: {
+        ':name': todoUpdate.name,
+        ':done': todoUpdate.done,
+        ':dueDate': todoUpdate.dueDate
+      },
+      ExpressionAttributeNames: { '#n': 'name' },
+      ReturnValues: 'UPDATED_NEW',
+    }).promise()
+  }
+
+  async deleteTodo(todoId: string, createdAt: string): Promise<void> {
+    this.docClient.delete({
+      TableName: this.todosTable,
+      Key: {
+        todoId,
+        createdAt
+      }
+    }).promise()
+  }
+
 }
 
 function createDynamoDBClient() {
