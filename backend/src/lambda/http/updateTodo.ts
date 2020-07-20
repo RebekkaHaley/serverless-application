7801696c1c89f2f9c9c1061be541b79b554f4fdc
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-20 21:30:32 +0100

    [http] added updateTodo.ts api functionality

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,39 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const logger = createLogger('updateTodoHandler')
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // Log API calls
+  logger.info('Update a todo for current user', event)
+
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+  // Get auth token for user
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  await updateTodo(todoId, updatedTodo, jwtToken)
+
+  return {
+    statusCode: 204,
+    body: ''
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
