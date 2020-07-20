a463c413607c68419f0e1c19cfb86dd1d4d4bbac
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-20 20:12:37 +0100

    [app] updated auth0 implementation

--- backend/src/auth/JwksClient.ts
+++ backend/src/auth/JwksClient.ts
@@ -1,27 +1,76 @@
 /**
- * https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/master/src/lib/JwksClient.js#L7-L28
+ * Code from: https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/master/src/lib/JwksClient.js
+ * and: https://github.com/hupe1980/serverless-todo/blob/master/backend/src/auth/JwksClient.ts
  **/
 
+import axios from 'axios';
+
+import { certToPEM } from './utils';
+import { CertSigningKey } from './CertSigningKey';
+import { Jwk } from './Jwk';
+
+let jwksCache: Jwk[] = [];
+
 export class JwksClient {
-    constructor(options) {
-      this.options = { strictSsl: true, ...options };
+    public constructor(private readonly jwksUrl: string) { }
+
+    async getJwks(): Promise<Jwk[]> {
+        if (jwksCache.length > 0) return jwksCache;
+
+        const res = await axios.get(this.jwksUrl);
+        const jwks = res.data.keys;
+
+        jwksCache = jwks;
+
+        return jwks;
     }
-  
-    getJwks(cb) {
-      request({
-        uri: this.options.jwksUri,
-        strictSsl: this.options.strictSsl,
-        json: true
-      }, (err, res) => {
-        if (err || res.statusCode < 200 || res.statusCode >= 300) {
-          if (res) {
-            return cb(new JwksError(res.body && (res.body.message || res.body) || res.statusMessage || `Http Error ${res.statusCode}`));
-          }
-          return cb(err);
+
+    async getSigningKeys(): Promise<CertSigningKey[]> {
+        const jwks = await this.getJwks();
+
+        if (!jwks || jwks.length === 0) {
+            throw new Error('The JWKS endpoint did not contain any keys');
         }
-  
-        var jwks = res.body.keys;
-        return cb(null, jwks);
-      });
+
+        const signingKeys = jwks
+            .filter(
+                (key): boolean =>
+                    key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+                    key.kty === 'RSA' && // We are only supporting RSA
+                    key.kid && // The `kid` must be present to be useful for later
+                    key.x5c &&
+                    key.x5c.length > 0 // Has useful public keys (we aren't using n or e)
+            )
+            .map(
+                (key): CertSigningKey => ({
+                    kid: key.kid,
+                    nbf: key.nbf,
+                    publicKey: certToPEM(key.x5c[0]),
+                }),
+            );
+
+        // If at least a single signing key doesn't exist we have a problem... Kaboom.
+        if (!signingKeys.length) {
+            throw new Error(
+                'The JWKS endpoint did not contain any signing keys',
+            );
+        }
+
+        // Returns all of the available signing keys.
+        return signingKeys;
     }
-  }
+
+    async getSigningKey(kid: string): Promise<CertSigningKey> {
+        const keys = await this.getSigningKeys();
+
+        const signingKey = keys.find((key): boolean => key.kid === kid);
+
+        if (!signingKey) {
+            throw new Error(
+                `Unable to find a signing key that matches '${kid}'`,
+            );
+        }
+
+        return signingKey;
+    }
+}
