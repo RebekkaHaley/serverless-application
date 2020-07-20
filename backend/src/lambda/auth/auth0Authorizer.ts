ad4b7ca5bdda15040e956227da87065f5b050dbb
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-20 20:26:32 +0100

    [app] fix whitespace and missing JwksClient

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,6 +6,7 @@ import { createLogger } from '../../utils/logger'
 // import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { JwksClient } from '../../auth/JwksClient'
 
 const logger = createLogger('auth')
 
@@ -63,7 +64,7 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const key = await jwksClient.getSigningKey(jwt.header.kid)
-  
+
   return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
 }
 
