4f3fc95d3ff647e521e404d999c3b8a6e614834e
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-25 17:58:12 +0100

    [app] updated api id to new ver

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // DONE: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'rvxbdsg4lk'
+const apiId = 'g65iy40yob'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
